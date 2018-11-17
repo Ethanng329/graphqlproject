d56ff7545520d805eebb5bf0e1d69c68b608f9ea
Ethan Ng <ethanng329@gmail.com>
2018-11-17 20:53:15 +0000

    basic GraphQL schema set up

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 const express = require('express');
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+//graphQL Schema
+const schema = buildSchema(
+  `
+  type Query {
+    message: String
+  }`
+);
+
+//Root resolver {
+const root = {
+  message: 'Hello World'
+};
+
 const app = express();
+
+app.use(
+  '/graphql',
+  express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(4000, console.log('listening to port 4000'));
